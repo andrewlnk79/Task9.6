f4795f687106c81b34d4c596093007b64b328399
Andrey Linnik <av.linnik79@ya.ru>
2023-03-25 14:57:17 +0300

    trable with bracets

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -4,14 +4,92 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Task1;
-
+//Создайте консольное приложение, в котором будет происходить сортировка списка фамилий из пяти человек.
+//Сортировка должна происходить при помощи события. Сортировка происходит при введении пользователем либо числа 1 (сортировка А-Я), либо числа 2 (сортировка Я-А).
+//Дополнительно реализуйте проверку введённых данных от пользователя через конструкцию TryCatchFinally с использованием собственного типа исключения.
 namespace Task2
 {
-    internal class Program 
+    internal class Program
     {
+
         static void Main(string[] args)
         {
-           
+          public delegate void A_Z();
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public List<string> CrateList()
+    {
+        List<string> list = new List<string>();
+        list.Add("Петров");
+        list.Add("Сидоров");
+        list.Add("Иванов");
+        list.Add("Васечкин");
+        list.Add("Алабаев");
+        return list;
+    }
+
+    public void SortA_Z(List<string> list)
+    {
+        if (list is null)
+        {
+            throw new ArgumentNullException(nameof(list));
+        }
+
+        foreach (var item in list)
+        {
+            list.Sort();
+            Console.WriteLine(item);
         }
     }
+    public void SortZ_A(List<string> list)
+    {
+        list.Reverse();
+        foreach (var item in list)
+        {
+            Console.WriteLine(item);
+        }
+    }
+    public void ReadFromConsole()
+    {
+
+        Console.WriteLine("ввевдите номер сортировки");
+        Console.WriteLine(" 1.- по алфовиту");
+        Console.WriteLine("2.- в обратном порядке");
+        try
+        {
+            var read = int.Parse(Console.ReadLine());
+            if (read == 1)
+            {
+
+
+            }
+
+        }
+        catch (Exception)
+        {
+            throw new MyExeption("вы неправильно ввели запрос");
+        }
+
+    }
+
 }
+}
+
+
+
+
